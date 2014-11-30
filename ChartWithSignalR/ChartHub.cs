97ecdca71986a68e6992ef1fdb71e20f1f6ed472
Elezium <elezium@hotmail.com>
2014-11-30 02:17:36 -0500

    Initial Commit

---
+++ ChartWithSignalR/ChartHub.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+
+namespace ChartWithSignalR
+{
+    public class ChartHub : Hub
+    {
+
+        public override Task OnConnected()
+        {
+            return Clients.All.Data(RandomData());            
+        }
+
+        public Task SendData()
+        {
+            return Clients.All.Data(RandomData());
+        }
+
+        private int RandomData()
+        {
+            Random r = new Random();
+            return r.Next(-10, 10);
+        }
+
+    }
+}
